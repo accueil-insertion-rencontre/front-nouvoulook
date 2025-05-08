be8cbb34fb7309de70395048a27be00e25eb078e
Martial59110 <floquet.martial@gmail.com>
2025-05-08 18:59:03 +0200

    feat(auth): add HTTP auth interceptor

---
+++ admin/src/app/services/auth.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService) {
+    console.log('AuthInterceptor instancié');
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.auth.getAccessToken();
+    console.log('Token trouvé dans l’interceptor :', token);
+    if (token) {
+      const cloned = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(cloned);
+    }
+    return next.handle(req);
+  }
+} 
