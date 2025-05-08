39c201ef9a705778664fca1ee03b46238a86d750
Martial59110 <floquet.martial@gmail.com>
2025-05-08 18:59:02 +0200

    refactor(routes): update application routes

--- admin/src/app/app.routes.ts
+++ admin/src/app/app.routes.ts
@@ -2,8 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './pages/guards/auth.guard';
+import { UserListComponent } from './pages/users/user-list.component';
+import { AffichageComponent } from './pages/affichage/affichage.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'admin', component: DashboardComponent, canActivate: [authGuard] }
+  { path: 'admin', component: DashboardComponent, canActivate: [authGuard],
+    children: [
+      { path: '', component: AffichageComponent },
+      { path: 'users', component: UserListComponent, canActivate: [authGuard] }
+    ]
+  }
 ];
