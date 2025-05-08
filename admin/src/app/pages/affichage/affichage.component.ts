179e456be81716fb532832a69b27933cc3e04dc9
Martial59110 <floquet.martial@gmail.com>
2025-05-08 18:59:03 +0200

    feat(affichage): add affichage page module

---
+++ admin/src/app/pages/affichage/affichage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-affichage',
+  standalone: true,
+  template: `<h2>Bienvenue sur la page d'affichage</h2>`
+})
+export class AffichageComponent {} 
