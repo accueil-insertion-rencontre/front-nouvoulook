a057a5930ac6ee0356bcf645b1085d0f9c7fd4d5
Martial59110 <floquet.martial@gmail.com>
2025-05-10 14:15:35 +0200

    feat(routing): update application routes

--- admin/src/app/app.routes.ts
+++ admin/src/app/app.routes.ts
@@ -3,7 +3,16 @@ import { authGuard } from './pages/guards/auth.guard';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+  },
+  {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin',
     loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
     canActivate: [authGuard],
     children: [
@@ -34,7 +43,7 @@ export const routes: Routes = [
     ]
   },
   {
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+    path: '**',
+    redirectTo: 'login'
   }
 ];
