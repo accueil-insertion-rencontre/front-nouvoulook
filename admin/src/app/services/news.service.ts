6f97b042d2c2b85aadf6c52cefc43b2890185ed3
Martial59110 <floquet.martial@gmail.com>
2025-05-15 20:39:43 +0200

    refactor(admin/news): use environment variable for api url

--- admin/src/app/services/news.service.ts
+++ admin/src/app/services/news.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface News {
   id: string;
@@ -12,7 +13,7 @@ export interface News {
 
 @Injectable({ providedIn: 'root' })
 export class NewsService {
-  private apiUrl = 'http://localhost:3001/news'; 
+  private apiUrl = `${environment.apiUrl}/news`; 
 
   constructor(private http: HttpClient) {}
 
